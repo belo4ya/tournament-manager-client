e30c6c0c6b9e42bd0399601ca66c3ef87b826170
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-04-23 23:56:43 +0300

    почти главная страничка

--- src/utils/routes.js
+++
@@ -1,8 +0,0 @@
-export const MAIN_ROUTE = '/';
-
-export const SIGNIN_ROUTE = '/auth/signin';
-export const REGISTER_ROUTE = '/auth/register';
-
-export const PROFILE_ROUTE = '/profile';
-export const TOURNAMENT_ROUTE = '/profile/tournaments';
-export const TEAM_ROUTE = '/profile/teams';
